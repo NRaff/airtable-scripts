bc7a6b1785759920f4995c2b2e8e0537da5e54b1
nraff <nraff510@gmail.com>
2022-02-01 13:47:01 -0800

    add order itemization

---
+++ WTC/itemizeOrderItems.js
@@ -0,0 +1,26 @@
+const { order, requestedItems, estSqFt, sqFeetName } = input.config()
+
+const itemizedOrders = base.getTable('⚡️ Order Items')
+const orderItems = requestedItems.map(item => {
+  return ({
+    fields: {
+      "Order": [{ id: order }],
+      "Menu Item": [{ id: item }]
+    }
+  })
+})
+
+const maxString = sqFeetName[0].split('= ')
+const maxSqFt = parseInt(maxString[1].split(',').join(''))
+
+if (sqFeetName[0].includes('>')) {
+  const numItems = Math.ceil((estSqFt - maxSqFt) / 500.00)
+  orderItems.push({
+    fields: {
+      "Order": [{ id: order }],
+      "Menu Item": [{ id: 'recy6cFujQ8xFb6dN' }],
+      "Number Ordered (if applicable)": numItems
+    }
+  })
+}
+await itemizedOrders.createRecordsAsync(orderItems)
