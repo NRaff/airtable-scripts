128e274e9daae7541743eaf4a9d459920fe1aafd
nraff <nraff510@gmail.com>
2022-02-07 11:18:30 -0800

    update order itemization

--- WTC/itemizeOrderItems.js
+++ WTC/itemizeOrderItems.js
@@ -10,17 +10,19 @@ const orderItems = requestedItems.map(item => {
   })
 })
 
-const maxString = sqFeetName[0].split('= ')
-const maxSqFt = parseInt(maxString[1].split(',').join(''))
+if (sqFeetName[0].includes('=')) {
+  const maxString = sqFeetName[0].split('= ')
+  const maxSqFt = parseInt(maxString[1].split(',').join(''))
 
-if (sqFeetName[0].includes('>')) {
-  const numItems = Math.ceil((estSqFt - maxSqFt) / 500.00)
-  orderItems.push({
-    fields: {
-      "Order": [{ id: order }],
-      "Menu Item": [{ id: 'recy6cFujQ8xFb6dN' }],
-      "Number Ordered (if applicable)": numItems
-    }
-  })
+  if (sqFeetName[0].includes('>')) {
+    const numItems = Math.ceil((estSqFt - maxSqFt) / 500.00)
+    orderItems.push({
+      fields: {
+        "Order": [{ id: order }],
+        "Menu Item": [{ id: 'recy6cFujQ8xFb6dN' }],
+        "Number Ordered (if applicable)": numItems
+      }
+    })
+  }
 }
 await itemizedOrders.createRecordsAsync(orderItems)
